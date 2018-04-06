846e626a03b6c21643853fd6fbbc7347c2d94ed6
Shih Han <swang05@mail.bbk.ac.uk>
2018-04-06 16:45:57 +0100

    player can't attack when HP less than 0

--- lib/game.rb
+++ lib/game.rb
@@ -1,19 +1,21 @@
 class Game
   attr_accessor :player_1, :player_2
-
+  ATTACK_HP = 15
   def initialize(player_1, player_2)
     @player_1 = Player.new(player_1)
     @player_2 = Player.new(player_2)
   end
 
-  # Recieves action from button click  
+  # Recieves action from button click
   def turn(action)
     @player = action.key?("1") ? @player_1 : @player_2
     reduceHP(@player)
   end
 
   private
+
   def reduceHP(player)
-    player.hp -= 20
+    player.hp -= ATTACK_HP if (player.hp - ATTACK_HP) >= 0
   end
+
 end
